299f14bdac0da5d928c4ede37bfbff3041d7ea21
Hariharan Ramanan <harryharanr@gmail.com>
2021-06-21 20:29:06 +0530

    timer logic has been moved to service

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
-
+import { TimerService } from './timer.service';
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
@@ -10,62 +10,14 @@ export class TimerComponent implements OnInit, OnDestroy {
   @Output() onComplete = new EventEmitter<void>();
   @Input() init:number = 20;
 
-  private countdownTimerRef:any = null;
-  public countdown:number = 0;
-  public paused: boolean = true;
-
-  constructor() { }
+  constructor(public timerService: TimerService) { }
 
   ngOnInit(): void {
-    if(this.init && this.init > 0) {
-      this.countdown = this.init;
-    }
+    this.timerService.restartCountdown(this.init);
   }
 
-  ngOnDestroy():void{
-    this.clearTimeout();
-  }
-
-  restartCountdown(){
-    if(this.init && this.init >0){
-      this.paused = true;
-      this.clearTimeout();
-      this.countdown = this.init;
-    }
-  }
-
-  toggleCountDown() {
-    this.paused = !this.paused;
-
-    if(!this.paused) {
-      this.doCountdown();
-    } else {
-      this.clearTimeout();
-    }
-  }
-
-  private doCountdown(){
-    this.countdownTimerRef = setTimeout(()=>{
-      this.countdown = this.countdown -1;
-      this.processCountdown();
-    }, 1000);
-  }
-
-  private processCountdown(){
-    if(this.countdown == 0){
-      this.onComplete.emit();
-      console.log("--countdown end--");
-    }
-    else{
-      this.doCountdown();
-    }
-  }
-
-  private clearTimeout(){
-    if(this.countdownTimerRef){
-      clearTimeout(this.countdownTimerRef);
-      this.countdownTimerRef = null;
-    }
+  ngOnDestroy() {
+    this.timerService.destroy();
   }
 
 }
