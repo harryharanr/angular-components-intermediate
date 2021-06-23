abee98972297998b0b4bc35fd04d86f53a8186db
Hariharan Ramanan <harryharanr@gmail.com>
2021-06-23 16:57:41 +0530

    Manipulating elements using ElementRef with @ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChildren, QueryList,  AfterContentInit, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import { Component, ViewChild, ViewChildren, QueryList,  AfterContentInit, AfterViewInit, ChangeDetectorRef, ElementRef } from '@angular/core';
 import { SimpleAlertViewComponent } from './simple-alert-view/simple-alert-view.component';
 
 @Component({
@@ -12,12 +12,16 @@ export class AppComponent implements AfterContentInit, AfterViewInit {
   public timers: Array<number> = [];
 
   @ViewChildren(SimpleAlertViewComponent) simpleAlerts: QueryList<SimpleAlertViewComponent>;
+  @ViewChild('timerInput') timerInput: ElementRef;
 
   constructor(private cdRef: ChangeDetectorRef) {
     this.timers = [3, 3, 185];
   }
 
   ngAfterViewInit() {
+    console.log(this.timerInput);
+    this.timerInput.nativeElement.setAttribute("placeholder","Enter Seconds");
+    this.timerInput.nativeElement.classList.add("time-in");
     this.simpleAlerts.forEach(item => {
       if(!item.title) {
         item.title = 'Hi';
@@ -39,6 +43,9 @@ export class AppComponent implements AfterContentInit, AfterViewInit {
 
   public showAddTimer() {
     this.isAddTimerVisible = true;
+    setTimeout(() => {
+      this.timerInput.nativeElement.focus();
+    });
   }
 
   public hideAddTimer() {
