7284de1834bf728d6ef295074f03d63b087a7bff
Hariharan Ramanan <harryharanr@gmail.com>
2021-06-23 17:08:47 +0530

    Using Renderer2 to manipulate the elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ViewChildren, QueryList,  AfterContentInit, AfterViewInit, ChangeDetectorRef, ElementRef } from '@angular/core';
+import { Component, ViewChild, ViewChildren, QueryList,  AfterContentInit, AfterViewInit, ChangeDetectorRef, ElementRef, Renderer2 } from '@angular/core';
 import { SimpleAlertViewComponent } from './simple-alert-view/simple-alert-view.component';
 
 @Component({
@@ -14,14 +14,17 @@ export class AppComponent implements AfterContentInit, AfterViewInit {
   @ViewChildren(SimpleAlertViewComponent) simpleAlerts: QueryList<SimpleAlertViewComponent>;
   @ViewChild('timerInput') timerInput: ElementRef;
 
-  constructor(private cdRef: ChangeDetectorRef) {
+  constructor(private cdRef: ChangeDetectorRef, private renderer: Renderer2) {
     this.timers = [3, 3, 185];
   }
 
   ngAfterViewInit() {
-    console.log(this.timerInput);
-    this.timerInput.nativeElement.setAttribute("placeholder","Enter Seconds");
-    this.timerInput.nativeElement.classList.add("time-in");
+    
+    this.renderer.setAttribute(this.timerInput.nativeElement, "placeholder", "Enter Seconds");
+    this.renderer.addClass(this.timerInput.nativeElement, "time-in");
+
+    // this.timerInput.nativeElement.setAttribute("placeholder","Enter Seconds");
+    // this.timerInput.nativeElement.classList.add("time-in");
     this.simpleAlerts.forEach(item => {
       if(!item.title) {
         item.title = 'Hi';
@@ -44,7 +47,7 @@ export class AppComponent implements AfterContentInit, AfterViewInit {
   public showAddTimer() {
     this.isAddTimerVisible = true;
     setTimeout(() => {
-      this.timerInput.nativeElement.focus();
+      this.renderer.selectRootElement(this.timerInput.nativeElement).focus();
     });
   }
 
